18c4f4be44026ba80ce5041334a3236bb2c20ffb
bryce <bturner46@gatech.edu>
2020-04-28 21:43:13 -0600

    Initial commit

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,17 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  //Values
+  val x : Int = 42
+  val theString : String = "String"
+  val daBoolean : Boolean = true
+  val daChar : Char = 'A'
+  val aShort : Short = 4061
+  val aLong : Long = 1234567890123456789L
+  val aFloat : Float = 1.3f
+  val aDouble: Double = 1.3123
+
+  //Variables, can be re-assigned
+  var firstVar : Int = 32
+  firstVar = 9
+}
